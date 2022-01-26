95d1d51a67178d769e336016d1af41843e7c1f16
ravikiran <ravikiransjce.jobs@gmail.com>
2022-01-26 13:20:49 +0530

    Added left and right buttons functionality to slide the images

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ images[1] = "2.jpeg";
 images[2] = "3.jpeg";
 images[3] = "4.jpeg";
 
+document.getElementById("sliderImg").src = images[0];
 function changeImage() {
   document.getElementById("sliderImg").src = images[i];
 
@@ -17,4 +18,27 @@ function changeImage() {
   }
 }
 
-setInterval(changeImage, time);
+// setInterval(changeImage, time);
+
+const leftBtn = document.querySelector(".leftBtn");
+const rightBtn = document.querySelector(".rightBtn");
+
+leftBtn.addEventListener("click", () => {
+  console.log("left clicked");
+  if (i === 0) {
+    i = images.length - 1;
+  } else {
+    i--;
+  }
+  document.getElementById("sliderImg").src = images[i];
+});
+
+rightBtn.addEventListener("click", () => {
+  console.log("left clicked");
+  if (i === images.length - 1) {
+    i = 0;
+  } else {
+    i++;
+  }
+  document.getElementById("sliderImg").src = images[i];
+});
