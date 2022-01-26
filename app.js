2575a1f5fbc1e9c6032390b4f3ab9b830c69defd
ravikiran <ravikiransjce.jobs@gmail.com>
2022-01-26 12:59:10 +0530

    created simple gallery without slider buttons, auto sliding on

---
+++ app.js
@@ -0,0 +1,20 @@
+let i = 0;
+let images = [];
+let time = 3000;
+
+images[0] = "1.jpeg";
+images[1] = "2.jpeg";
+images[2] = "3.jpeg";
+images[3] = "4.jpeg";
+
+function changeImage() {
+  document.getElementById("sliderImg").src = images[i];
+
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+}
+
+setInterval(changeImage, time);
